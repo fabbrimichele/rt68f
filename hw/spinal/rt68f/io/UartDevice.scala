6df7cadafdc79de1f08bf2d5851d8e82db7a090a
Michele Fabbri <fabbrimichele75@gmail.com>
2025-10-22 22:27:19 +0200

    Comment.

--- hw/spinal/rt68f/io/UartDevice.scala
+++ hw/spinal/rt68f/io/UartDevice.scala
@@ -29,20 +29,8 @@ case class UartDevice() extends Component {
   producer.valid := CounterFreeRun(2000).willOverflow
   producer.payload := B('a',8 bits)
 
-
+  // Without fifo, it doesn't work
   val fifo = StreamFifo(Bits(8 bits), depth = 4)
   producer >> fifo.io.push
   fifo.io.pop >> uartCtrl.io.write
-
-  /*
-  val write = Stream(Bits(8 bits))
-  write.valid := CounterFreeRun(2000).willOverflow
-  // TODO: implement transmission as first step
-  //  1. create a register for data out
-  //  2. create a control register (ready to send, etc...)
-  //  configuration (baud, stop, etc.) fixed for the time being
-  write.payload := B('a', 8 bits)
-  write >-> uartCtrl.io.write
-
-   */
 }
