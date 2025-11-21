3c9daf5dabfa81334ecfcd1af0388ee7cb40a01b
Michele Fabbri <fabbrimichele75@gmail.com>
2025-11-22 00:14:10 +0100

    Parametrize video mode

--- hw/spinal/rt68f/vga/VgaDevice.scala
+++ hw/spinal/rt68f/vga/VgaDevice.scala
@@ -9,9 +9,32 @@ import spinal.lib.{master, slave}
 
 import scala.language.postfixOps
 
+case class VgaMode(
+  yScale: Int,
+  xWordScale: Int,
+  pixelsPerWord: Int,
+  bitsPerPixel: Int,
+)
+
 object VgaDevice {
   val rgbConfig = RgbConfig(4, 4, 4)
+
+  object Modes {
+    val mode640x400x2 = VgaMode(
+      yScale = 0,
+      xWordScale = 4,
+      pixelsPerWord = 16,
+      bitsPerPixel = 1,
+    )
+    val mode640x200x4 = VgaMode(
+      yScale = 1,
+      xWordScale = 3,
+      pixelsPerWord = 8,
+      bitsPerPixel = 2,
+    )
+  }
 }
+
 
 //noinspection TypeAnnotation
 case class VgaDevice() extends Component {
@@ -26,6 +49,13 @@ case class VgaDevice() extends Component {
   // TODO: add a CTRL register to switch between:
   //  - Actual resolution: 640x400 2 colors and 640x200 4 colors (consider also 320x200)
   //  - Monitor resolution: 640x480 (no black bands) and 640x400
+
+  // Vga mode
+  // TODO: select it from the control register
+  //       Note this is probably not enough, I might need a Mux
+  //       to switch between values in several places in the code
+  //val mode = VgaDevice.Modes.mode640x200x4
+  val mode = VgaDevice.Modes.mode640x400x2
 
   // Framebuffer
   val size = 32768 / 2  // 32KB = 640x400, 1 bit color
@@ -146,26 +176,24 @@ case class VgaDevice() extends Component {
     )
 
     // 5. Vertical Clamp: Ensure the address does not exceed VRAM height (200 lines).
-    //val vramLastLine = U(399, pixelY.getWidth bits)
     val vramLastLine = U(399, pixelY.getWidth bits)
     val pastVramLines = pixelY > vramLastLine
 
     val vramY = Mux(
       pastVramLines,
       vramLastLine,
-      //pixelY
-      pixelY(11 downto 1)
+      pixelY(11 downto mode.yScale)
     )
 
     // 6. VRAM X Word Address: (pixelX) divided by 16
     //    RAM needs to be read one pixel earlier to
     //    compensate for the read requiring one clock.
     //val vramXWord = (pixelX + 1)(pixelX.high downto 4) // 640x400x2 = pixel/16 -> 1 bits per pixel, 1 word = 16 pixels
-    val vramXWord = (pixelX + 1)(pixelX.high downto 3) // 640x200x4 = pixelX/8 -> 2 bits per pixel, 1 word = 8 pixels
+    val vramXWord = (pixelX + 1)(pixelX.high downto mode.xWordScale) // 640x200x4 = pixelX/8 -> 2 bits per pixel, 1 word = 8 pixels
 
     // 7. Linear Address = (Y_clamped * 40) + X_word
     //val lineLength = U(640 / 16)    // 640x400, 2 colors = 16 pixels per word = 40 words per line
-    val lineLength = U(640 / 8)    // 640x200, 4 colors = 8 pixels per word = 80 words per line
+    val lineLength = U(640 / mode.pixelsPerWord)    // 640x200, 4 colors = 8 pixels per word = 80 words per line
     val addressWidth = log2Up(size) // 13 bits (for 8192 words)
     val vramAddress = ((vramY * lineLength) + vramXWord).resize(addressWidth)
 
@@ -175,22 +203,27 @@ case class VgaDevice() extends Component {
       clockCrossing = true
     )
 
-    val shiftRegister = Reg(Bits(16 bits)) init(0)
+    val shiftRegister = Reg(Bits(16 bits)) init 0
 
     //val pixelBitIndex = pixelX(3 downto 0) // 640x400, 2 colors -> 16 pixels per word
-    val pixelBitIndex = pixelX(2 downto 0) // 640x200, 4 colors -> 8 pixels per word
+    val pixelBitIndex = pixelX((mode.xWordScale - 1) downto 0) // 640x200, 4 colors -> 8 pixels per word
     when (pixelBitIndex === 0) {
       shiftRegister := wordData
     } otherwise {
-      shiftRegister := shiftRegister |<< 2
+      shiftRegister := shiftRegister |<< mode.bitsPerPixel
     }
 
-    val pixelDataBit = shiftRegister(15 downto 14)
+    // val pixelDataBit = shiftRegister(15 downto 15)
+    val pixelDataBit = shiftRegister(15 downto (16 - mode.bitsPerPixel))
 
     ctrl.io.rgb.clear()
 
+    // TODO: something is not working...
+    //  try to load the image 4 color it
+    //  should be shown dithered but there are
+    //  black columns
     when(ctrl.io.vga.colorEn && !pastVramLines) {
-      val selectedColor = palette(pixelDataBit.asUInt)
+      val selectedColor = palette(pixelDataBit.asUInt.resized)
       ctrl.io.rgb.r := selectedColor(11 downto 8)
       ctrl.io.rgb.g := selectedColor(7 downto 4)
       ctrl.io.rgb.b := selectedColor(3 downto 0)
