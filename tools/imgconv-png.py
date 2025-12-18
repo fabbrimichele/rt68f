136b6e64f8d16b01f9cf05cea2cdc652267efb44
Michele Fabbri <fabbrimichele75@gmail.com>
2025-12-18 15:55:34 +0100

    Add converted to PNG 320x200

---
+++ tools/imgconv-png.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from PIL import Image
+import sys
+
+from PIL import Image
+import sys
+
+def adjust_image(img):
+    img = img.convert("RGB")  # Ensure RGB mode
+    img = img.resize((320, 200), Image.Resampling.LANCZOS)
+    # Quantize to 16 colors (Indexed mode)
+    # 'P' mode is indexed, 'palette=Image.ADAPTIVE' finds the best 16 colors
+    img = img.quantize(colors=16, method=Image.Quantize.MAXCOVERAGE)
+    return img
+
+def convert(in_filename, out_filename):
+    """Convert a PNG image to 320x200 pixels 16 colors with palette"""
+    img = Image.open(in_filename)
+    img = adjust_image(img)
+    img.save(out_filename)
+
+if len(sys.argv) == 3:
+    in_filename = sys.argv[1]
+    out_filename = sys.argv[2]
+    print(f"Converting to PNG 320x200 pixel 16 colors...")
+    convert(in_filename, out_filename)
+    print(f"Done")
+else:
+    print(f"Converts an image to a PNG image [320x200 pixels, 16 colors]")
+    print(f"usage: imgconv-png input_filename output_filename")
