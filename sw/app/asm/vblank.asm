b48e7272c0f73513ec8efcba51dcf9e985150254
Michele Fabbri <fabbrimichele75@gmail.com>
2026-01-11 19:44:28 +0100

    Show a counter on the LEDs in the interrupt test program.

--- sw/app/asm/vblank.asm
+++ sw/app/asm/vblank.asm
@@ -4,8 +4,9 @@
     ORG    $400             ; Start of RAM
 
 START:
-    MOVE.B  #INT_CNT,COUNTER    ; Reset counter
-    MOVE.W  #$FFFF,LED          ; Init LED
+    MOVE.B  #INT_CNT,DELAY      ; Reset delay
+    MOVE.B  #0,COUNTER          ; Reset counter
+    MOVE.W  COUNTER,LED         ; Init LED
     MOVE.L  #VBL_ISR,VT_INT_1   ; Set interrupt handler
     OR.W    #$0008,VGA_CTRL     ; Enable VBlank interrupt on VGA (bit 3 high)
     AND.W   #$F8FF,SR           ; Enable all interrupts on 68000 (Clear mask bits)
@@ -13,15 +14,17 @@ START:
 
 VBL_ISR:
     OR.W    #$0040,VGA_CTRL     ; Ack interrupt (write high to bit 6)
-    SUBQ.B  #1,COUNTER
+    SUBQ.B  #1,DELAY
     BNE     .RET
-    MOVE.B  #INT_CNT,COUNTER    ; Reset counter
-    NOT.W   LED
+    MOVE.B  #INT_CNT,DELAY      ; Reset DELAY
+    ADDQ.B  #1,COUNTER
+    MOVE.W  COUNTER,LED
 .RET:
     RTE
 
 ; Variables
-COUNTER     ds.b    1
+DELAY       ds.b    1
+COUNTER     ds.w    1
 
 ; Constants
 INT_CNT     equ     30
