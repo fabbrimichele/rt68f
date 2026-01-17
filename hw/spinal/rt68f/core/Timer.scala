d7b30482dbf7a7949f8188bb03f870d9c428f284
Michele Fabbri <fabbrimichele75@gmail.com>
2026-01-17 19:19:35 +0100

    Additional timer registers

--- hw/spinal/rt68f/core/Timer.scala
+++ hw/spinal/rt68f/core/Timer.scala
@@ -5,6 +5,21 @@ import spinal.lib._
 
 import scala.language.postfixOps
 
+/**
+ * # Control Register (8 bits)
+ * Bit 0: Timer A mode: 0 -> repeat, 1 -> single
+ * Bit 1: Timer B mode: 0 -> repeat, 1 -> single
+ * Bit 2: Timer A int : 0 -> off, 1 -> on
+ * Bit 3: Timer B int : 0 -> off, 1 -> on
+ * Bit 6: Timer A ack : Write to acknowledge Timer A interrupt
+ * Bit 7: Timer B ack : Write to acknowledge Timer B interrupt
+ *
+ * # Prescaler (8 bits)
+ * It divides the clock by the set value, 0 stop counter
+ *
+ * # Init
+ */
+//noinspection TypeAnnotation
 case class Timer() extends Component {
   val io = new Bundle {
     val bus       = slave(M68kBus())
@@ -13,12 +28,14 @@ case class Timer() extends Component {
     val timerBInt = out Bool()        // Timer B interrupt
   }
 
-  // --------- Registers ---------
-  val ctrlReg    = Reg(Bits(8 bits))  init 0
-  val prescalerA = Reg(Bits(16 bits)) init 0
-  val counterA   = Reg(Bits(16 bits)) init 0
-  val prescalerB = Reg(Bits(16 bits)) init 0
-  val counterB   = Reg(Bits(16 bits)) init 0
+  // -- Memory Mapped Registers --
+  val ctrlReg    = Reg(Bits(8 bits))  init 0 // Read/Write
+  val initPrescA = Reg(Bits(8 bits))  init 0 // Read/Write
+  val initValueA = Reg(Bits(16 bits)) init 0 // Write only
+  val counterA   = Reg(Bits(16 bits)) init 0 // Read only
+  val initPrescB = Reg(Bits(8 bits))  init 0 // Read/Write
+  val initValueB = Reg(Bits(16 bits)) init 0 // Write only
+  val counterB   = Reg(Bits(16 bits)) init 0 // Read only
 
   // ---- 68000 Bus Interface ----
   // Default bus signals
@@ -33,9 +50,9 @@ case class Timer() extends Component {
       // Read
       io.bus.DATAI := addrWord.mux(
         0 -> ctrlReg.resize(16),
-        1 -> prescalerA,
+        1 -> initPrescA.resize(16),
         2 -> counterA,
-        3 -> prescalerB,
+        3 -> initPrescB.resize(16),
         4 -> counterB,
         default -> B"xFFFF"
       )
@@ -45,13 +62,11 @@ case class Timer() extends Component {
       val dataOut = io.bus.DATAO
       switch(addrWord) {
         is(0) { ctrlReg := dataOut(7 downto 0) }
-        is(1) { prescalerA := dataOut }
-        is(2) { counterA := dataOut }
-        is(3) { prescalerB := dataOut }
-        is(4) { counterB := dataOut }
+        is(1) { initPrescA := dataOut(7 downto 0) }
+        is(2) { initValueA := dataOut }
+        is(3) { initPrescB := dataOut(7 downto 0) }
+        is(4) { initValueB := dataOut }
       }
     }
   }
-
-  // TODO: implement the counters and interrupts
 }
