ff080cada16aa2b7111b6f37ca38a25c8f3ede27
Michele Fabbri <fabbrimichele75@gmail.com>
2026-01-18 16:10:38 +0100

    Fix tabs

--- sw/lib/asm/timer.asm
+++ sw/lib/asm/timer.asm
@@ -1,11 +1,9 @@
-TMRA      EQU     $00405000 ; Timer A Base Address
-TMRA_CTRL EQU     TMRA      ; Control Register
-TMRA_PRS  EQU     TMRA+2    ; Prescaler A
-TMRA_CNT  EQU     TMRA+4    ; Counter A
+TMRA      EQU   $00405000 ; Timer A Base Address
+TMRA_CTRL EQU   TMRA      ; Control Register
+TMRA_PRS  EQU   TMRA+2    ; Prescaler A
+TMRA_CNT  EQU   TMRA+4    ; Counter A
 
-TMRB      EQU     $00405010 ; Timer B Base Address
-TMRB_CTRL EQU     TMRB      ; Control Register
-TMRB_PRS  EQU     TMRB+2    ; Prescaler B
-TMRB_CNT  EQU     TMRB+4    ; Counter B
-
-
+TMRB      EQU   $00405010 ; Timer B Base Address
+TMRB_CTRL EQU   TMRB      ; Control Register
+TMRB_PRS  EQU   TMRB+2    ; Prescaler B
+TMRB_CNT  EQU   TMRB+4    ; Counter B
