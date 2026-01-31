ad213d4e44e3430184ea56d2c759406d4eb8ad35
Michele Fabbri <fabbrimichele75@gmail.com>
2026-01-31 23:17:40 +0100

    PS/2 A and B test programs

--- sw/lib/asm/ps2.asm
+++ sw/lib/asm/ps2.asm
@@ -1,5 +1,7 @@
-; TODO: define the PS/2 A
+PS2A      EQU   $00406000 ; PS/2 A Base Address
+PS2A_CTRL EQU   PS2A      ; Control Register
+PS2A_DATA EQU   PS2A+2    ; Data Register
 
-PS2B      EQU   $00406000 ; PS/2 B Base Address
+PS2B      EQU   $00406004 ; PS/2 B Base Address
 PS2B_CTRL EQU   PS2B      ; Control Register
 PS2B_DATA EQU   PS2B+2    ; Data Register
