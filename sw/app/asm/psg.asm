09244edf3c2b8cd8a81bca556f6243e184d20db8
Michele Fabbri <fabbrimichele75@gmail.com>
2026-02-16 23:39:23 +0100

    Write to PSG from memory (no 68000 registers)

--- sw/app/asm/psg.asm
+++ sw/app/asm/psg.asm
@@ -4,38 +4,28 @@
     ORG    $400             ; Start of RAM
 
 START:
-    MOVE.W  #$00,D0         ; Channel A Fine Tune
-    MOVE.W  #$FE,D1
-    BSR     PSG_WRITE
+    MOVE.W  #$00,PSG_ADDR   ; Channel A Fine Tune
+    MOVE.W  #$FE,PSG_DATA
 
-    MOVE.W  #$02,D0         ; Channel A Coarse Tune
-    MOVE.W  #$00,D1
-    BSR     PSG_WRITE
+    MOVE.W  #$00,PSG_ADDR   ; Channel A Fine Tune
+    MOVE.W  #$FE,PSG_DATA
 
-    MOVE.W  #$07,D0         ; Mixer Control
-    MOVE.W  #$7E,D1         ; Bit 0 (Tone A) must be zero (active low)
-    BSR     PSG_WRITE
+    MOVE.W  #$02,PSG_ADDR   ; Channel A Coarse Tune
+    MOVE.W  #$00,PSG_DATA
+
+    MOVE.W  #$07,PSG_ADDR   ; Mixer Control
+    MOVE.W  #$7E,PSG_DATA   ; Bit 0 (Tone A) must be zero (active low)
 
     ; TODO: check how this register is suppose to work
     ; This command doesn't work, it mute the volume
-    MOVE.W  #08,D0          ; Channel A Amplitude
-    MOVE.W  #0F,D1          ; Set to maximum volume
-    ;BSR     PSG_WRITE
+    ;MOVE.W  #08,PSG_ADDR   ; Channel A Amplitude
+    ;MOVE.W  #0F,PSG_DATA   ; Set to maximum volume
 
     BSR     DELAY
-    MOVE.W  #$07,D0         ; Mixer Control
-    MOVE.W  #$00,D1         ; Mute all channels
-    BSR     PSG_WRITE
+    MOVE.W  #$07,PSG_ADDR   ; Mixer Control
+    MOVE.W  #$00,PSG_DATA   ; Mute all channels
 
     TRAP    #14
-
-; Write to PSG registers
-; D0 register
-; D1 data
-PSG_WRITE:
-    MOVE.W  D0,PSG_ADDR
-    MOVE.W  D1,PSG_DATA
-    RTS
 
 DELAY:
     MOVE.L  #DLY_VAL,D3     ;
